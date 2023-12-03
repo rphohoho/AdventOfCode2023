634dfa2aa1c93663e8b65251e9fc4d2ad15be58b
rphohoho <vincentlai998@gmail.com>
2023-12-02 23:59:23 -0800

    Complete Day3

--- Day03/src/Day3.java
+++ Day03/src/Day3.java
@@ -17,7 +17,8 @@ public class Day3{
 			e.printStackTrace();
 		}
 		char[][] input = list.toArray(new char[0][0]);
-		part1(input);
+		//part1(input);
+		part2(input);
 	}
 
 	private void part1(char[][] input){
@@ -51,6 +52,83 @@ public class Day3{
 			}
 		}
 		return false;
+	}
+
+	private void part2(char[][] input){
+		int sum = 0;
+		int row = input.length, col = input[0].length;
+		Map<Coordinate, Gear> gears = new HashMap<>();
+		for(int r = 0; r < row; r++){
+			for(int c = 0; c < col; c++){
+				char curr = input[r][c];
+				if(curr < '0' || curr > '9')
+					continue;
+				int num = curr-'0';
+				int end = c;
+				while(end+1 < col && input[r][end+1] >= '0' && input[r][end+1] <= '9'){
+					curr = input[r][++end];
+					num = num*10+(curr-'0');
+				}
+				checkNumber(r, c, end, num, input, gears);
+				c = end+1;
+			}
+		}
+		for(Gear g : gears.values())
+			if(g.num == 2)
+				sum += g.ratio;
+		System.out.println(sum);
+	}
+
+	private void checkNumber(int row, int start, int end, int val, char[][] input, Map<Coordinate, Gear> gears){
+		for(int r = Math.max(0, row-1); r < Math.min(input.length, row+2); r++){
+			for(int c = Math.max(0, start-1); c < Math.min(input[0].length, end+2); c++){
+				char curr = input[r][c];
+				if(curr == '*'){
+					Gear temp = gears.getOrDefault(new Coordinate(r,c), new Gear());
+					temp.add(val);
+					gears.put(new Coordinate(r,c), temp);
+				}
+			}
+		}
+	}
+
+	private class Gear{
+		int ratio;
+		int num;
+
+		public Gear(){
+			ratio = 1;
+			num = 0;
+		}
+
+		public void add(int val){
+			ratio *= val;
+			num++;
+		}
+	}
+
+	private class Coordinate{
+		final int x, y;
+		int hashCode;
+
+		public Coordinate(int x, int y){
+			this.x = x;
+			this.y = y;
+			this.hashCode = Objects.hash(x, y);
+		}
+
+		public boolean equals(Object o){
+			if(this == o)
+				return true;
+			if(!(o instanceof Coordinate))
+				return false;
+			Coordinate c = (Coordinate) o;
+			return x == c.x && y == c.y;
+		}
+
+		public int hashCode(){
+			return this.hashCode;
+		}
 	}
 
 	public static void main(String[] args){

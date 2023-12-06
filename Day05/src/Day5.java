5927a21b4868655733ec3179b18f8de08361ee18
rphohoho <vincentlai998@gmail.com>
2023-12-05 20:34:13 -0800

    Complete Day5
    Took too long for part2

--- Day05/src/Day5.java
+++ Day05/src/Day5.java
@@ -95,18 +95,104 @@ public class Day5{
 		catch(FileNotFoundException e){
 			e.printStackTrace();
 		}
-		for(long k : humidityToLocation.keySet()){
-			System.out.print(k + " ");
-			for(long v : humidityToLocation.get(k))
-				System.out.print(v + " ");
-			System.out.println();
-		}
 		//part1();
+		part2();
 	}
 
 	private void part1(){
 		long minLocation = Long.MAX_VALUE;
 		for(long seed : seeds){
+			long soil = findDestination(seed, seedToSoil);
+			long fertilizer = findDestination(soil, soilToFertilizer);
+			long water = findDestination(fertilizer, fertilizerToWater);
+			long light = findDestination(water, waterToLight);
+			long temperature = findDestination(light, lightToTemperature);
+			long humidity = findDestination(temperature, temperatureToHumidity);
+			long location = findDestination(humidity, humidityToLocation);
+			minLocation = Math.min(minLocation, location);
+		}
+		System.out.println(minLocation);
+	}
+
+	private void part2(){
+		long minLocation = Long.MAX_VALUE;
+		for(int i = 0; i < seeds.length; i += 2){
+			long seed = seeds[i];
+			for(long range = seeds[i+1]; range > 0 ; range--){
+				long soil = findDestination(seed, seedToSoil);
+				long fertilizer = findDestination(soil, soilToFertilizer);
+				long water = findDestination(fertilizer, fertilizerToWater);
+				long light = findDestination(water, waterToLight);
+				long temperature = findDestination(light, lightToTemperature);
+				long humidity = findDestination(temperature, temperatureToHumidity);
+				long location = findDestination(humidity, humidityToLocation);
+				minLocation = Math.min(minLocation, location);
+				seed++;
+			}
+		}
+		System.out.println(minLocation);
+	}
+
+	private long findDestination(long num, TreeMap<Long, long[]> map){
+		long source;
+		try{
+			source = map.floorKey(num);
+		}
+		catch(NullPointerException e){
+			return num;
+		}
+		long destination = map.get(source)[0];
+		long range = map.get(source)[1];
+		if(num < source+range)
+			return destination+(num-source);
+		return num;
+	}
+
+	private void checkInputValid(){
+		for(long k : seeds)
+			System.out.print(k + " ");
+		System.out.println();
+		for(long k : seedToSoil.keySet()){
+			System.out.print(k + " ");
+			for(long v : seedToSoil.get(k))
+				System.out.print(v + " ");
+			System.out.println();
+		}
+		for(long k : soilToFertilizer.keySet()){
+			System.out.print(k + " ");
+			for(long v : soilToFertilizer.get(k))
+				System.out.print(v + " ");
+			System.out.println();
+		}
+		for(long k : fertilizerToWater.keySet()){
+			System.out.print(k + " ");
+			for(long v : fertilizerToWater.get(k))
+				System.out.print(v + " ");
+			System.out.println();
+		}
+		for(long k : waterToLight.keySet()){
+			System.out.print(k + " ");
+			for(long v : waterToLight.get(k))
+				System.out.print(v + " ");
+			System.out.println();
+		}
+		for(long k : lightToTemperature.keySet()){
+			System.out.print(k + " ");
+			for(long v : lightToTemperature.get(k))
+				System.out.print(v + " ");
+			System.out.println();
+		}
+		for(long k : temperatureToHumidity.keySet()){
+			System.out.print(k + " ");
+			for(long v : temperatureToHumidity.get(k))
+				System.out.print(v + " ");
+			System.out.println();
+		}
+		for(long k : humidityToLocation.keySet()){
+			System.out.print(k + " ");
+			for(long v : humidityToLocation.get(k))
+				System.out.print(v + " ");
+			System.out.println();
 		}
 	}
 

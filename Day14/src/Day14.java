6f7686baae6adf84abd2928abdf1d089e8de9b63
rphohoho <vincentlai998@gmail.com>
2023-12-21 21:19:07 -0800

    Complete Day14

--- Day14/src/Day14.java
+++ Day14/src/Day14.java
@@ -19,7 +19,8 @@ public class Day14{
 		catch(FileNotFoundException e){
 			e.printStackTrace();
 		}
-		part1(inputs.toArray(char[][]::new));
+		//part1(inputs.toArray(char[][]::new));
+		part2(inputs.toArray(char[][]::new));
 	}
 
 	private void part1(char[][] platform){
@@ -32,6 +33,71 @@ public class Day14{
 					stoneLoad = len-r-1;
 				else if(platform[r][c] == 'O')
 					sum += stoneLoad--;
+		}
+		System.out.println(sum);
+	}
+
+	private void part2(char[][] platform){
+		int row = platform.length;
+		int col = platform[0].length;
+		//the rocks move in cycle for every 175 spins,
+		//thus only need 1_000_000_000%175 = 125 spins
+		for(int i = 1; i <= 125; i++){
+			spin(platform, col, row);
+		}
+		loadSum(platform, row, col);
+	}
+
+	private void spin(char[][] platform, int row, int col){
+		for(int c = 0; c < col; c++){
+			int stopsRock = 0;
+			for(int r = 0; r < row; r++)
+				if(platform[r][c] == '#')
+					stopsRock = r+1;
+				else if(platform[r][c] == 'O'){
+					platform[r][c] = '.';
+					platform[stopsRock++][c] = 'O';
+				}
+		}
+		for(int r = 0; r < row; r++){
+			int stopsRock = 0;
+			for(int c = 0; c < col; c++)
+				if(platform[r][c] == '#')
+					stopsRock = c+1;
+				else if(platform[r][c] == 'O'){
+					platform[r][c] = '.';
+					platform[r][stopsRock++] = 'O';
+				}
+		}
+		for(int c = 0; c < col; c++){
+			int stopsRock = row-1;
+			for(int r = row-1; r >= 0; r--)
+				if(platform[r][c] == '#')
+					stopsRock = r-1;
+				else if(platform[r][c] == 'O'){
+					platform[r][c] = '.';
+					platform[stopsRock--][c] = 'O';
+				}
+		}
+		for(int r = 0; r < row; r++){
+			int stopsRock = col-1;
+			for(int c = col-1; c >= 0; c--)
+				if(platform[r][c] == '#')
+					stopsRock = c-1;
+				else if(platform[r][c] == 'O'){
+					platform[r][c] = '.';
+					platform[r][stopsRock--] = 'O';
+				}
+		}
+	}
+
+	private void loadSum(char[][] platform, int row, int col){
+		int sum = 0;
+		for(int r = 0; r < row; r++){
+			int stoneLoad = row-r;
+			for(int c = 0; c < col; c++)
+				if(platform[r][c] == 'O')
+					sum += stoneLoad;
 		}
 		System.out.println(sum);
 	}

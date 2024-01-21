bd40c57f8bd206af69fe431d9d2a1e5831d91d7d
rphohoho <vincentlai998@gmail.com>
2024-01-20 18:27:49 -0800

    Complete Day18

--- Day18/src/Day18.java
+++ Day18/src/Day18.java
@@ -18,7 +18,8 @@ public class Day18{
 		catch(FileNotFoundException e){
 			e.printStackTrace();
 		}
-		part1(plan);
+		//part1(plan);
+		part2(plan);
 	}
 
 	private void part1(LinkedList<String[]> plan){
@@ -34,8 +35,8 @@ public class Day18{
 				case "L" -> {exterior += width; 
 							 if(lastDirection.equals("D")) exterior++; 
 							 distFromOrigin -= width;}
-				case "R" -> distFromOrigin += width;
-			}
+				case "R" ->  distFromOrigin += width;
+			};
 			lastDirection = direction;
 			
 			direction = plan.peek()[0];
@@ -44,7 +45,38 @@ public class Day18{
 				case "D" -> {exterior += height; 
 							 if(lastDirection.equals("L")) exterior--;
 							 res += distFromOrigin*height;} 
-				case "U" ->	res += distFromOrigin*-height;
+				case "U" ->	 res += distFromOrigin*-height;
+			};
+			lastDirection = direction;
+		}
+
+		System.out.println(Math.abs(res)+exterior);
+	}
+
+	private void part2(LinkedList<String[]> plan){
+		long res = 0;
+		long distFromOrigin = 0;
+		long exterior = 0;
+		int lastDirection = 3; //0=R, 1=D, 2=L, 3=U
+
+		while(!plan.isEmpty()){
+			int direction = Integer.parseInt(plan.peek()[2].substring(7,8));
+			long width = Long.parseLong(plan.poll()[2].substring(2,7), 16);
+			switch(direction){
+				case 2 -> {exterior += width; 
+						   if(lastDirection == 1) exterior++; 
+						   distFromOrigin -= width;}
+				case 0 ->  distFromOrigin += width;
+			};
+			lastDirection = direction;
+			
+			direction = Integer.parseInt(plan.peek()[2].substring(7,8));
+			long height = Long.parseLong(plan.poll()[2].substring(2,7), 16);
+			switch(direction){
+				case 1 -> {exterior += height; 
+						   if(lastDirection == 2) exterior--;
+						   res += distFromOrigin*height;} 
+				case 3 ->  res += distFromOrigin*-height;
 			};
 			lastDirection = direction;
 		}
